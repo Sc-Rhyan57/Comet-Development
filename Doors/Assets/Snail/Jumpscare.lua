70f612124e112e3cd1abed9a737b82ac3b22ce4c
Chrono <150987804+ChronoAcceleration@users.noreply.github.com>
2024-09-26 17:19:02 -0700

    Update Jumpscare.lua
    - Fix

--- Doors/Assets/Snail/Jumpscare.lua
+++ Doors/Assets/Snail/Jumpscare.lua
@@ -1,9 +1,12 @@
+-- Property of @dripocapy Uploaded by @notchron
+
 local FUNNYJUMPSCARELOL = Instance.new("ScreenGui")
 local Frame = Instance.new("Frame")
 local ImageLabel = Instance.new("ImageLabel")
 
 FUNNYJUMPSCARELOL.Name = "FUNNY JUMPSCARE LOL"
 FUNNYJUMPSCARELOL.Parent = game.CoreGui
+FUNNYJUMPSCARELOL.IgnoreGuiInset = true
 FUNNYJUMPSCARELOL.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
 Frame.Parent = FUNNYJUMPSCARELOL
@@ -22,34 +25,43 @@ ImageLabel.Position = UDim2.new(0.5, 0, 0.5, 0)
 ImageLabel.Size = UDim2.new(0.0500000007, 0, 0.100000001, 0)
 ImageLabel.Image = "rbxassetid://8318773866"
 
--- Scripts:
+local Jumpscare = Instance.new("Sound", FUNNYJUMPSCARELOL)
+Jumpscare.SoundId = "rbxassetid://5567523008"
+Jumpscare.Volume = 1
+Jumpscare.Name = "Jumpscare"
 
-local function EZEQDfake_script() -- FUNNYJUMPSCARELOL.LocalScript 
-    local script = Instance.new('LocalScript', FUNNYJUMPSCARELOL)
+local Jumpscare2 = Instance.new("Sound", FUNNYJUMPSCARELOL)
+Jumpscare2.SoundId = "http://www.roblox.com/asset/?id=11984351"
+Jumpscare2.Volume = 1
+Jumpscare2.Name = "Jumpscare2"
+Jumpscare2.PlaybackSpeed = 2
 
-    script.Parent.Jumpscare:Play()
-    game.TweenService:Create(script.Parent.Frame.ImageLabel, TweenInfo.new(0.15), {Size = UDim2.fromScale(1,1)}):Play()
-
-    task.wait(1)
-
-    for ,v in pairs(script.Parent:GetDescendants()) do
-        if v:IsA("ImageLabel") or v:IsA("Frame") then
-            game.TweenService:Create(v, TweenInfo.new(1), {BackgroundTransparency = 0.5}):Play()
-        end
-    end
-
-    task.wait(1)
-
-    script.Parent.Jumpscare:Stop()
-    script.Parent.Jumpscare2:Play()
-    script.Parent.Frame.BackgroundTransparency = 0
-    script.Parent.Frame.ImageLabel.ImageTransparency = 0
-    script.Parent.Frame.ImageLabel.BackgroundTransparency = 0
-    script.Parent.Frame.ImageLabel.Image = "http://www.roblox.com/asset/?id=3124557274"
-
-
-    task.wait(3)
-
-    script.Parent:Destroy()
+local function EZEQD_fake_script()
+	local script = Frame
+	
+	script.Parent.Jumpscare:Play()
+	game.TweenService:Create(script.Parent.Frame.ImageLabel, TweenInfo.new(0.15), {Size = UDim2.fromScale(1,1)}):Play()
+	
+	task.wait(1)
+	
+	for _,v in pairs(script.Parent:GetDescendants()) do
+		if v:IsA("ImageLabel") or v:IsA("Frame") then
+			game.TweenService:Create(v, TweenInfo.new(1), {BackgroundTransparency = 0.5}):Play()
+		end
+	end
+	
+	task.wait(1)
+	
+	script.Parent.Jumpscare:Stop()
+	script.Parent.Jumpscare2:Play()
+	script.Parent.Frame.BackgroundTransparency = 0
+	script.Parent.Frame.ImageLabel.ImageTransparency = 0
+	script.Parent.Frame.ImageLabel.BackgroundTransparency = 0
+	script.Parent.Frame.ImageLabel.Image = "http://www.roblox.com/asset/?id=3124557274"
+	
+	
+	task.wait(3)
+	
+	script.Parent:Destroy()
 end
-coroutine.wrap(EZEQD_fake_script)() 
+coroutine.wrap(EZEQD_fake_script)()
